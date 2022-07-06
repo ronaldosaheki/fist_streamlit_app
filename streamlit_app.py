3cc803c7e1d8705911487e3f94abddeded486641
Ronaldo <rsaheki@gmail.com>
2022-07-06 14:24:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("Tutorial Healthy Dinner App")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omage 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
